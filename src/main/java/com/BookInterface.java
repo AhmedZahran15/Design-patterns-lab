b4c6f7e628e21ae2df8d6665ab5b83e7d6dfd3cb
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-05-17 17:08:59 +0300

    Create base models for Library system with Book and User classes

--- src/main/java/com/BookInterface.java
+++ src/main/java/com/BookInterface.java
@@ -1,6 +1,9 @@
 package com;
 
 public interface BookInterface {
-     void borrowBook(User user) ;
-     void returnBook() ;
+    void borrowBook(User user);
+    void returnBook();
+    boolean isAvailable();
+    String getTitle();
+    void setAvailable(boolean available);
 }
