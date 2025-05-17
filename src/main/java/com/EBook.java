e2ba6d66cdacb9c19c7a96b3e405b01f55d94ee7
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-05-17 17:09:06 +0300

    Implement different book types (Physical, Electronic, Historical)

--- src/main/java/com/EBook.java
+++ src/main/java/com/EBook.java
@@ -1,8 +1,28 @@
 package com;
-public class EBook extends Book{
+
+public class EBook extends Book implements ElectronicBookInterface {
+    private boolean requiresPremiumAccess = true;
 
     public EBook(String title) {
         super(title);
     }
-
+    
+    @Override
+    public void borrowBook(User user) {
+        if (user.isPremium() || !requiresPremiumAccess) {
+            super.borrowBook(user);
+            System.out.println(" e-book");
+        } else {
+            System.out.println("Only premium users can borrow e-books.");
+        }
+    }
+    
+    @Override
+    public boolean requiresPremiumAccess() {
+        return requiresPremiumAccess;
+    }
+    
+    public void setRequiresPremiumAccess(boolean requiresPremiumAccess) {
+        this.requiresPremiumAccess = requiresPremiumAccess;
+    }
 }
